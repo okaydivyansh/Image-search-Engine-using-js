3be9d2984abdc304bac84422e1034a323c1d384b
Divyansh Mishra <divyanshm09@gmail.com>
2023-12-18 15:23:50 +0530

    yuva

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
+const key="mKtuijjIz7sk787_3uoo0UHDaKfeMpiza2a_9enLB7Y";
 const searchForm=document.getElementById("search-form");
 const searchBox=document.getElementById("search-box");
 const searchResult=document.getElementById("search-result");
-const searchMoreBtn=document.getElementById("search-more-btn");
-let keyword="";
+const searchMoreBtn=document.getElementById("show-more-btn");
+var keyword="";
 let page=1;
-const key="mKtuijjIz7sk787_3uoo0UHDaKfeMpiza2a_9enLB7Y"
 async function searchImages()
 {
     keyword=searchBox.value;
-    const url=`https://api.unsplash.com/photos?page=${page}&query=${keyword}&client_id=${key}`
+    const url=`https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${key}&per_page=12`;
+    const response= await fetch(url);
+    const data=await response.json();
+    console.log(data);
+    const results=data.results;
+    results.map((result)=>
+    {
+        const image=document.createElement("img");
+        image.src=result.urls.small;
+        const imageLink=document.createElement("a");
+        imageLink.href=result.links.html;
+        imageLink.target="_blank";
+        imageLink.appendChild(image);
+        searchResult.appendChild(imageLink);
+    })
+    searchMoreBtn.style.display="block";
 }
+searchForm.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    page=1;
+    searchImages();
+})
+searchMoreBtn.addEventListener("click",()=>
+    {
+        page++;
+    }
+)
